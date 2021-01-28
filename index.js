bc3bdcda0257747ad0bfed3ba145b878a8cc0412
dewabrata <dewabrata@gmail.com>
2021-01-28 15:47:29 +0700

    add category

--- index.js
+++ index.js
@@ -9,8 +9,26 @@ app.use(cors())
 
 router.get("/quiz",(req,res,next)=>{
    
- 
+   console.log(req.query.type);
+   console.log(data);
+   switch(req.query.type){
+   
+   case "bebas" :
+   res.json(data.data[0].data)
+   break
+   
+   case "hitungan" :
+   res.json(data.data[1].data)
+   break
+   
+   default :
    res.json(data.data)
+   break;
+   
+   
+   }
+   
+  
    next()
 
 })
--- model/Model.js
+++ model/Model.js
@@ -1,6 +1,28 @@
 
+let soal_hitungan = JSON.parse(`[{
+	"type": "radio",
+	"soal": "Siapakah penemu baling-baling bambu",
+	"a": "Doraemon",
+	"b": "hanif",
+	"c": "nobita",
+	"d": "naruto",
+	"kunci": "a",
+	"score": 20
+}, {
+	"type": "checkbox",
+	"soal": "pilihlah 3 yang benar mengenai aqil",
+	"option": ["Trainer Juara Coding", "Manusia", "Anggota Avenger"],
+	"kunci": ["Manusia", "Anggota Avenger"],
+	"score": 20
+}, {
+	"type": "essay",
+	"soal": "Buat gambar bintang lima dengan menggunakan coding javascript looping",
+	"kunci": "*****",
+	"score": 20
+}]`);
 
-module.exports.data = JSON.parse(`[{
+
+let soal_bebas = JSON.parse(`[{
 	"type": "radio",
 	"soal": "Siapakah penemu baling-baling bambu",
 	"a": "Doraemon",
@@ -45,3 +67,5 @@ module.exports.data = JSON.parse(`[{
 	"kunci": "a",
 	"score": 20
 }]`);
+
+module.exports.data= [{type : "bebas", data: soal_bebas, image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2lHaLRIdiBEQctEaWhJPDDn6nPJH0rwm95w&usqp=CAU",description:"Soal pengetahuan umum"}, {type:"hitungan", data:soal_hitungan, image:"https://cdn.idntimes.com/content-images/community/2019/08/calculator-385506-960-720-5762849e0ede2c709ca3c39cd7f4ca73_600x400.jpg",description:"Kumpulan soal-soal hitungan"}]
